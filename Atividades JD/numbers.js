fd18a4aa9b50d8c8cf3e78e4c0b75e916e85d208
vinizk1 <128698338+vinizk1@users.noreply.github.com>
2024-02-23 11:54:39 -0300

    Add files via upload

---
+++ Atividades JD/numbers.js
@@ -0,0 +1,9 @@
+let inteiro = 10;
+let flutuante = 11.9;
+let inteiro2 = 20;
+let flutuante2 = 5.65
+
+console.log(inteiro);
+console.log(inteiro2);
+console.log(flutuante);
+console.log(flutuante2);
--- Atividades JD/string.js
+++ Atividades JD/string.js
@@ -1,6 +1,25 @@
-let nome = "Carlitos"
-let nome2 = 'Romeu'
-let nomes = `O nome de ${nome} é ${nome}`
+let nome = "Carlitos";
+let nome2 = 'Romeu';
+let nomes = `O nome de ${nome} é ${nome}`;
 console.log (nome)
 console.log (nome2)
 console.log (nomes)
+
+let endereco = "Cidade do free faire";
+let cachorro1 = "belinha";
+let cachorro2 = "Outra belinha";
+let cachorro3 = "Belinha2";
+console.log(`O nome do cachorro 1 é ${cachorro1}`);
+console.log(`O nome do cachorro 2 é ${cachorro2}`);
+console.log(`O nome do cachorro 3 é ${cachorro3}`);
+console.log(`O meu nome não é ${nome}`);
+console.log(`Eu moro na ${endereco}`);
+
+let inteiro = 10;
+let flutuante = 11.9;
+let inteiro2 = 20;
+let flutuante2 = 5.65
+console.log(typeof(inteiro));
+console.log(typeof(inteiro2));
+console.log(typeof(flutuante));
+console.log(typeof(flutuante2));
